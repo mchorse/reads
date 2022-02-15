9b0106d8e7997f3a832036e31bd45077f959f238
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-02-15 11:36:43 +0100

    Update the banner message to something more maintainer friendly

--- templates/banner.php
+++ templates/banner.php
@@ -1,5 +1,5 @@
 <?php if (isset($abandoned)): ?> 
-**ATTENTION**: This mod is **no longer being developed nor maintained by McHorse**.<?php if ($abandoned === 'open'): ?> If you wish to continue the mod, feel free to check out the [source code](<?php echo $links[$project]['github'] ?>). <?php endif ?>
+**ATTENTION**: This mod is **no longer being maintained by McHorse.**<?php if ($abandoned === 'open'): ?> However there are **still going to be official updates by others** and there have been task forces that are investigating on porting it. **If you also want to contribute to official updates** or to the porting effort, please **join the official [McHorse Discord Server](<?php echo $links['discord'] ?>) so we can communicate with you.** Checkout the [source code](<?php echo $links[$project]['github'] ?>). <?php endif ?>
 <?php endif ?> 
 
 <?php if ($domain !== \mchorse\PMC): ?> 
